94bd86f07c159d921228272b5ad2ee5cbe862032
Michael Phelps <michaeljphelps1978@gmail.com>
2020-09-11 13:11:51 -0500

    :recycle: Game Loop
    Refactors game loop to use new methods and classes from previous refactor commits.

--- src/adv.py
+++ src/adv.py
@@ -1,48 +1,15 @@
 from room import Room
 from player import Player
 from inventory import Inventory
-
-
-# Declare all the rooms
-
-room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons. Just inside the entrance, you notice a torch giving off a faint light",
-                     [Inventory(
-                         "Torch", "It\s a torch. What do you think it does\?")],
-                     isLit=True
-                     ),
-
-    'foyer':    Room("Foyer",
-                     "Dim light filters in from the south. Dusty passages run north and east.",
-                     [Inventory(
-                         "Note", "The note is written in what appears to be ancient runes. You are able to decipher it: My friends, what is here is yours")],
-                     isLit=True
-                     ),
-
-    'overlook': Room("Grand Overlook",
-                     """A steep cliff appears before you, falling into the darkness. Ahead to the north, you can make out a doorway, but there is no way across the chasm.""",
-                     isLit=True
-                     ),
-
-    'narrow':   Room("Narrow Passage",
-                     """The narrow passage bends here from west to north. The smell of gold permeates the air.""",
-                     [Inventory(
-                         "Key", "This is a silver key encrusted with diamonds")],
-                     isLit=False
-                     ),
-
-    'treasure': Room("Treasure Chamber",
-                     """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south.""",
-                     [Inventory(
-                         "Chest", "There is a large, open chest in the center of the room. Sadly, it is empty.")],
-                     isLit=False
-                     )
-}
+from item import Item
+from item import Treasure
+from item import LightSource
+from room_list import room
+from item_list import item_list
+# There's got to be an easier way to do this...
 
 
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -51,6 +18,8 @@ room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
+room['workroom'].s_to = room['overlook']
+room['secret'].s_to = room['treasure']
 
 #
 # Main
@@ -66,6 +35,89 @@ no_path = 'You can\'t go that way!!'
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
+
+def game_loop():
+    name = input("What shall I call you?\n")
+    player = Player(name, room['outside'])
+    end = False
+    win = False
+
+    print(f"Welcome, brave {player.name} - Your destiny awaits!")
+    selected = None
+
+    while end == False and win == False:
+        # Activate Secret Rooms!
+        if player.location.new_passage == True:
+            if player.location.name == 'Grand Overlook':
+                room['overlook'].n_to = room['workroom']
+            if player.location.name == 'Treasure Chamber':
+                room['treasure'].n_to = room['secret']
+
+        # define win condition
+        if player.money >= 10000:
+            win = True
+            break
+
+        # Check lightsources and adjust the room light accordingly (ie, do we have a torch?)
+        if item_list['torch'] in player.inventory:
+            player.location.illuminate()
+        else:
+            player.location.deluminate()
+
+        print("\n")
+
+        # If secret room discovered, we need to get the new description of the room
+        if player.location.new_passage == True:
+            print(player.location.alt_desc)
+        else:
+            print(player.location.description)
+
+        action = input("What would you like to do?\n\n")
+        split = action.split()
+
+        # Quitting the game
+        if action == 'q' or action == 'quit':
+            end = True
+
+        # Search the room
+        elif action == 'search':
+            player.location.search_room()
+
+        # movement
+        elif action == 'n' or action == 's' or action == 'e' or action == 'w':
+            player.location = player.move(action)
+
+        # Item use
+        elif len(split) > 1:
+            selected = split[1]
+            if 'get' in action:
+                player.take_item(item_list[selected])
+            elif 'drop' in action:
+                player.drop_item(item_list[selected])
+            elif 'use' in action:
+                player.use_item(item_list[selected])
+            elif 'examine' in action:
+                if selected != 'room':
+                    player.examine_item(item_list[selected])
+                else:
+                    player.current_room.describe()
+
+        # if they just say examine, it confuses us
+        elif action == 'examine':
+            print('What do you want to look at?')
+
+        # inventory check
+        elif action == "inventory" or action == 'i':
+            player.print_inventory()
+
+        # the only possible commands
+        elif action in ['help', 'h', '?']:
+            print(
+                "Travel or search by using 't' or 's'. Travel 'n' 's' 'e' or 'w' .\nGet an item with 'get [name of item]' and drop with 'drop [name of item]. Use 'q' or 'quit' to end the game.\n")
+        else:
+            print(
+                "I don't understand what you mean. Use 'h' to see list of available options")
+
 
 while True:
     character_location = character.location
